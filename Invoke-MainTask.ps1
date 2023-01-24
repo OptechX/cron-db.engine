6c99b4cb829172bebcda204b2d88029ceb7d59b7
danijeljw-rpc <danijel@repasscloud.com>
2023-01-25 00:30:58 +1100

    update structure

--- Invoke-MainTask.ps1
+++ Invoke-MainTask.ps1
@@ -5,49 +5,63 @@
 if (Test-Path -Path $LONG_RECORD_OUTPUT) { Remove-Item -Path $LONG_RECORD_OUTPUT -Force -Confirm:$false }
 $json = Get-Content -Path $PSScriptRoot/lib/windows_index.json | ConvertFrom-Json
 
-#region Windows 10
-[System.String]$KEY = "Windows 10"
-$DATA = $json.$KEY
-$RELEASE = $DATA.'RELEASE'
+$KEYS = $json.WINDOWS_LIST
 
-<# WRITE LONG RECORD #>
-foreach ($r in $RELEASE)
+foreach ($k in $KEYS)
 {
-    <# api:version->$r #>
-    "${KEY} ${r}" | Out-File -FilePath $LONG_RECORD_OUTPUT -Append
-    $EDITION = $DATA.$r.'EDITION'
-    foreach ($e in $EDITION)
+    "${k}" | Out-File -FilePath $LONG_RECORD_OUTPUT -Append
+    $RELEASE = $json.$k.'RELEASE'
+    foreach ($r in $RELEASE)
     {
-        "  - ${e}" | Out-File -FilePath $LONG_RECORD_OUTPUT -Append
-        foreach ($ARCH in $DATA.$r.$e.'Arch')
+        "  - ${r}" | Out-File -FilePath $LONG_RECORD_OUTPUT -Append
+        $EDITION = $json.$k.$r.'EDITION'
+        foreach ($e in $EDITION)
         {
-            "    - ${ARCH}" | Out-File -FilePath $LONG_RECORD_OUTPUT -Append
-            foreach ($LANG in $DATA.$r.$e.'Lang')
+            "    - ${e}" | Out-File -FilePath $LONG_RECORD_OUTPUT -Append
+            $ARCH = $json.$k.$r.$e.'ARCH'
+            foreach ($a in $ARCH)
             {
-                "      - ${LANG}" | Out-File -FilePath $LONG_RECORD_OUTPUT -Append
+                "      - ${a}" | Out-File -FilePath $LONG_RECORD_OUTPUT -Append
+                $EOL = $json.$k.$r.$e.'EOL'
+                $BUILD = $json.$k.$r.$e.'BUILD'
+                $LANG = $json.$k.$r.$e.'LANG'
+                foreach ($l in $LANG)
+                {
+                    "        - ${l}" | Out-File -FilePath $LONG_RECORD_OUTPUT -Append
+                }
             }
         }
     }
 }
 
-<# PROCESS WINDOWS IMAGE #>
-# foreach ($r in $RELEASE)
-# {
-#     <# api:version->$r (ie 21H2) #>
-#     foreach ($e in $DATA.$r.'EDITION')
-#     {
-#         <# api:edition->$e.Split(',') (ie Professional/Pro)#>
-#         foreach ($a in $DATA.$r.$e.'ARCH')
-#         {
-#             Write-Output "Downloading: ${KEY} ${r} ${e} ${a} - English"
-#             if ($IsWindows) { $URL = & "$PSScriptRoot\Fido\Fido.ps1" -Win "$KEY" -Rel "$r" -Ed "$e" -Lang "English" -Arch "$a" -GetUrl }
-#             else { $URL = & "$PSScriptRoot/Fido/Fido.ps1" -Win "$KEY" -Rel "$r" -Ed "$e" -Lang "English" -Arch "$a" -GetUrl }
-#             "$URL"
-#             $FILE = Split-Path -Path $URL.Split('?')[0] -Leaf
-#             $FILE
-#             # Invoke-WebRequest -Uri $URL -OutFile "./data/${FILE}" -UseBasicParsing -SslProtocol Tls13 -Method Get -ContentType "application/json"
-#             Pause
-#         }
-#     }
-# }
-#endregion Windows 10
+
+
+# #region Windows 10
+# [System.String]$KEY = "Windows 10"
+# $DATA = $json.$KEY
+# $RELEASE = $DATA.'RELEASE'
+
+# <# WRITE LONG RECORD #>
+
+
+# <# PROCESS WINDOWS IMAGE #>
+# # foreach ($r in $RELEASE)
+# # {
+# #     <# api:version->$r (ie 21H2) #>
+# #     foreach ($e in $DATA.$r.'EDITION')
+# #     {
+# #         <# api:edition->$e.Split(',') (ie Professional/Pro)#>
+# #         foreach ($a in $DATA.$r.$e.'ARCH')
+# #         {
+# #             Write-Output "Downloading: ${KEY} ${r} ${e} ${a} - English"
+# #             if ($IsWindows) { $URL = & "$PSScriptRoot\Fido\Fido.ps1" -Win "$KEY" -Rel "$r" -Ed "$e" -Lang "English" -Arch "$a" -GetUrl }
+# #             else { $URL = & "$PSScriptRoot/Fido/Fido.ps1" -Win "$KEY" -Rel "$r" -Ed "$e" -Lang "English" -Arch "$a" -GetUrl }
+# #             "$URL"
+# #             $FILE = Split-Path -Path $URL.Split('?')[0] -Leaf
+# #             $FILE
+# #             # Invoke-WebRequest -Uri $URL -OutFile "./data/${FILE}" -UseBasicParsing -SslProtocol Tls13 -Method Get -ContentType "application/json"
+# #             Pause
+# #         }
+# #     }
+# # }
+# #endregion Windows 10
