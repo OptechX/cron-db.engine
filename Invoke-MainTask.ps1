1981e0cb2935e54e880924b9aa4a5fe0f56fe10f
danijeljw-rpc <danijel@repasscloud.com>
2023-01-25 18:24:51 +1100

    ordinance configure

--- Invoke-MainTask.ps1
+++ Invoke-MainTask.ps1
@@ -1,5 +1,6 @@
 <# VARIABLES #>
 [System.String]$LONG_RECORD_OUTPUT = "${PSScriptRoot}/long-record-output.txt"
+$OFS = ', '
 
 <# PRE-PROCESSING #>
 if (Test-Path -Path $LONG_RECORD_OUTPUT) { Remove-Item -Path $LONG_RECORD_OUTPUT -Force -Confirm:$false }
@@ -9,14 +10,14 @@ $json = Get-Content -Path $PSScriptRoot/lib/windows_index.json | ConvertFrom-Jso
 class WindowsCoreIdentity {
     [System.Int16]$id = 0
     [System.Guid]$uuid = [System.Guid]::NewGuid()
-    [System.String]$uid
-    [System.String]$release
-    [System.String]$edition
-    [System.String]$version
-    [System.String]$build
-    [System.String]$arch
-    [System.String]$windowsLcid
-    [System.String]$supportedUntil
+    [System.String]$uid             # 
+    [System.String]$release         # Windows 10
+    [System.String]$edition         # Enterprise/LTSC
+    [System.String]$version         # 21H2
+    [System.String]$build           # 190444
+    [System.String]$arch            # x64
+    [System.String]$windowsLcid     # MUI
+    [System.String]$supportedUntil  # 2022-10-23
 }
 
 $KEYS = $json.WINDOWS_LIST
@@ -32,11 +33,38 @@ foreach ($k in $KEYS)
         foreach ($e in $EDITION)
         {
             <# CREATE THE OBJECT HERE#>
-            $wci = [WindowsCoreIdentity]::new()
-            $wci.release = $KEY
-            wci.edition = $EDITION
-            wci.
+            $lcid = $json.$k.$r.$e.'LANG'
+            $cpuarch = $json.$k.$r.$e.'ARCH'
+            [System.String]$gciLcid = ""
+            switch($lcid.Count)
+            {
+                { $_ -eq 1 } {
+                    $gciLcid = "en-US"
+                }
+                { $_ -gt 1 } {
+                    $gciLcid = "en-US,MUI"
+                }
+            }
 
+            $wci = [WindowsCoreIdentity]::new()
+            $wci.release = $k
+            $wci.edition = $e
+            $wci.version = $r
+            $wci.build = $json.$k.$r.$e.'BUILD'
+            $wci.arch = "$cpuarch"
+            $wci.windowsLcid = "$lcid"
+            $wci.supportedUntil = $json.$k.$r.$e.'EOL'.ToString()
+            $uid = $wci.release + '.' `
+                    + $wci.edition + '.' `
+                    + $wci.version + '.' `
+                    + $wci.build + '.' `
+                    + "$cpuarch".ToString() + '.' `
+                    + $gciLcid
+            $uid = $uid.Replace(' ','').Replace(',','').ToLower().Replace("en-usmui","en-us_mui")
+            $wci.uid = $uid
+            $body = $wci | ConvertTo-Json
+            $body
+            Invoke-RestMethod -Uri "https://engine.api.prod.optechx-data.com/v1/WindowsCoreIdentity" -Method Post -UseBasicParsing -Body $body -Header @{"Accept" = "application/json"} -ContentType "application/json"
 
 
             "    - ${e}" | Out-File -FilePath $LONG_RECORD_OUTPUT -Append
